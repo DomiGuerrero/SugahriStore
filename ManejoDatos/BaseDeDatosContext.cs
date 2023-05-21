27740ed257af71632c0dbf62e27fe2e7aabc4e72
Domi <Domi@DESKTOP-V1QUR2A>
2023-05-21 11:19:27 +0200

    add auditoria y modify context

--- ManejoDatos/BaseDeDatosContext.cs
+++ ManejoDatos/BaseDeDatosContext.cs
@@ -12,6 +12,7 @@ namespace SugahriStore.ManejoDatos
         public DbSet<LineaPedido> LineasPedido { get; set; }
         public DbSet<Producto> Productos { get; set; }
         public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Auditoria> Auditorias { get; set; }
         public DbSet<Rol> Roles { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -30,9 +31,11 @@ namespace SugahriStore.ManejoDatos
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Pedido>().ToTable("Pedidos")
-             .HasMany(p => p.LineasPedido)
-             .WithOne(lp => lp.Pedido)
-             .HasForeignKey(lp => lp.PedidoId);
+                .HasMany(p => p.LineasPedido)
+                .WithOne(lp => lp.Pedido)
+                .HasForeignKey(lp => lp.PedidoId);
+
+            modelBuilder.Entity<LineaPedido>().ToTable("LineasPedido");
 
             modelBuilder.Entity<Producto>().ToTable("Productos");
             modelBuilder.Entity<Producto>(entity =>
@@ -46,12 +49,15 @@ namespace SugahriStore.ManejoDatos
                 entity.HasKey(e => e.Id);
             });
 
+            modelBuilder.Entity<Auditoria>().ToTable("Auditorias");
+
             modelBuilder.Entity<Rol>().ToTable("Roles")
-            .HasMany(r => r.Usuarios) 
-            .WithOne(u => u.Rol)
-            .HasForeignKey(u => u.RolId);
+                .HasMany(r => r.Usuarios)
+                .WithOne(u => u.Rol)
+                .HasForeignKey(u => u.RolId);
 
             base.OnModelCreating(modelBuilder);
         }
+
     }
 }
